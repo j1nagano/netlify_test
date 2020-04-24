fd361512e5014de47e7f2c9fe113cbc2e5888824
Junichi Nagano <naganoj@intellilink.co.jp>
2020-04-24 16:46:26 +0900

    debug

--- src/reply.js
+++ src/reply.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-exports.handler = async function(event, context, callback){
+exports.handler = async function(event, context, callback) {
   const webhookBody = JSON.parse(event.body)
   console.log(webhookBody)
 
   const data = {
     replyToken: webhookBody.events[0].replyToken,
-    message: [
+    messages: [
       {
         type: 'text',
         text: 'Hello Netlify Bot'
@@ -14,12 +14,13 @@ exports.handler = async function(event, context, callback){
     ]
   }
 
-  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data,{
+  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
     }
   })
+  console.log(res)
 
   callback(null, {
     statusCode: 200,
